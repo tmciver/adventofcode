e666581c1f995682619a4a2900d192fd3cab27e8
Tim McIver <tim@timmciver.com>
2017-12-03 18:11:48 -0500

    Added test for correct answer to day 1, 2017.

--- test/Y2017/D01Test.hs
+++ test/Y2017/D01Test.hs
@@ -7,6 +7,7 @@ import Y2017.D01
 tests :: TestTree
 tests = testGroup "Tests for day 1, 2017" [ testCaptcha
                                           , testStringToInts
+                                          , testAnswer
                                           ]
 
 testCaptcha :: TestTree
@@ -21,3 +22,10 @@ testStringToInts :: TestTree
 testStringToInts = testGroup "`stringToInts` function test" $
                    [ testCase "test success" $ stringToInts "123" @?= [1,2,3]
                    , testCase "test failure" $ stringToInts "123a" @?= []]
+
+testAnswer :: TestTree
+testAnswer = testCase "answer test" $ do
+  s <- readFile "test/Y2017/D01Input.txt"
+  let answer = captcha $ stringToInts (init s)
+      expectedAnswer = 1182
+  answer @?= expectedAnswer

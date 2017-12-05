9858885c1f31174772901adf4f95c5cbe1ff5f19
Tim McIver <tim@timmciver.com>
2017-12-05 12:03:41 -0500

    Refactored the captcha solution to parameterize the "pair-making" function in preparation for part 2.

--- src/Y2017/D01.hs
+++ src/Y2017/D01.hs
@@ -44,16 +44,43 @@ the next.
 * 91212129 produces 9 because the only digit that matches the next one
 is the last digit, 9.
 
+--- Part Two ---
+
+You notice a progress bar that jumps to 50% completion. Apparently, the door
+isn't yet satisfied, but it did emit a star as encouragement. The instructions
+change:
+
+Now, instead of considering the next digit, it wants you to consider the digit
+halfway around the circular list. That is, if your list contains 10 items, only
+include a digit in your sum if the digit 10/2 = 5 steps forward matches
+it. Fortunately, your list has an even number of elements.
+
+For example:
+
+* 1212 produces 6: the list contains 4 items, and all four digits match the
+digit 2 items ahead.
+* 1221 produces 0, because every comparison is between a 1 and a 2.
+* 123425 produces 4, because both 2s match each other, but no other digit has a
+match.
+* 123123 produces 12.  12131415 produces 4.
+
+What is the solution to your new captcha?
+
 -}
 
 import Data.Char (isDigit, digitToInt)
 
-captcha :: [Int] -> Int
-captcha = sum . (map fst) . (filter equalPair) . pairs
+type MkPairs a = [a] -> [(a, a)]
+
+captcha :: (Num a, Eq a) => MkPairs a -> [a] -> a
+captcha pairs = sum . (map fst) . (filter equalPair) . pairs
+  where equalPair :: Eq a => (a, a) -> Bool
+        equalPair (x, y) = x == y
+
+captcha1 :: (Num a, Eq a) => [a] -> a
+captcha1 = captcha pairs
   where pairs :: [a] -> [(a, a)]
         pairs l = zip l (tail (l ++ l))
-        equalPair :: Eq a => (a, a) -> Bool
-        equalPair (x, y) = x == y
 
 charToInt :: Char -> Maybe Int
 charToInt c = if isDigit c
--- test/Y2017/D01Test.hs
+++ test/Y2017/D01Test.hs
@@ -5,17 +5,17 @@ import Test.Tasty.HUnit
 import Y2017.D01
 
 tests :: TestTree
-tests = testGroup "Tests for day 1, 2017" [ testCaptcha
+tests = testGroup "Tests for day 1, 2017" [ testCaptcha1
                                           , testStringToInts
                                           , testAnswer1
                                           ]
 
-testCaptcha :: TestTree
-testCaptcha = testGroup "`captcha` function test"
-              [ testCase "example 1" $ captcha [1,1,2,2] @?= 3
-              , testCase "example 2" $ captcha [1,1,1,1] @?= 4
-              , testCase "example 3" $ captcha [1,2,3,4] @?= 0
-              , testCase "example 4" $ captcha [9,1,2,1,2,1,2,9] @?= 9
+testCaptcha1 :: TestTree
+testCaptcha1 = testGroup "`captcha1` function test"
+              [ testCase "example 1" $ captcha1 [1,1,2,2] @?= 3
+              , testCase "example 2" $ captcha1 [1,1,1,1] @?= 4
+              , testCase "example 3" $ captcha1 [1,2,3,4] @?= 0
+              , testCase "example 4" $ captcha1 [9,1,2,1,2,1,2,9] @?= 9
               ]
 
 testStringToInts :: TestTree
@@ -26,6 +26,6 @@ testStringToInts = testGroup "`stringToInts` function test" $
 testAnswer1 :: TestTree
 testAnswer1 = testCase "answer test" $ do
   s <- readFile "test/Y2017/TestInput/D01Input.txt"
-  let answer = captcha $ stringToInts (init s)
+  let answer = captcha1 $ stringToInts (init s)
       expectedAnswer = 1182
   answer @?= expectedAnswer
